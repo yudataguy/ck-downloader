282b6be5ae950c4c4cdb651f8fcc837a208a892d
yudataguy <yudataguy@gmail.com>
2017-10-05 19:39:49 -0700

    Add files via upload
    Add new files and make changes to README

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# CuteKorean-Downloader
-CuteKorean SubReddit Album Downloader
+# CuteKorean Downloader
+
+An easy-to-use /r/CuteKorean subReddit album download bot. It will automatically download all latest upload, or base on your preference, only download selected ones.
+
+**Python3** Required. For beginner, please read "[Python Installation Guide](http://docs.python-guide.org/en/latest/starting/installation/)" first. 
+
+## How to run the bot 
+
+There are two files available, **cqrssdl.py** and **crqssdl_ala.py**.
+
+**cqrssdl.py** only downloads selected models. You can edit `<models_name>` list to add or remove selections. 
+
+**cqrssdl.py** downloads all the latest albums. 
+
+### Run command
+
+Please execute the following in your terminal: 
+>python cqrssdl.py
+>python cqrssdl_ala.py
+
+## How to automate the download
+
+You can automate this bot with Windows Task Scheduler, Linux Cron, and Mac Crontab. I'm unable to find a good guide on this. Please google it yourself in the mean time. 
+
+## Note
+
+This is only the prototype. More feature, including GUI is planned. Please feel free to make changes to the bot. 
+
+## License
+
+MIT
+
+## Thanks
+
+alexgisby: [imgur Album Downloader](https://github.com/alexgisby/imgur-album-downloader)
+jtara1: [Imgur Downloader](https://github.com/jtara1/imgur_downloader/tree/master/imgurdownloader)
---
+++ cqrssdl.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Album downloader for /r/CuteKorean subreddit
+
+@author: You
+
+Ver. 0.04
+10/01/2017 - Need fix folder issue, and add try/except for TypeError
+10/02/2017 - Fixed folder and TypeError, next create "a la carte" version
+10/03/2017 - Need fix a-la-carte download issue.
+10/04/2017 - All bugs fixed, ready for public beta
+
+Description: Reddit /r/CuteKorean album download bot. Automatically download
+all the latest albums. 
+
+Thanks to "imguralbum" and "imgurdownloader" scripts' authors for making this 
+bot possible. 
+
+License: MIT
+"""
+
+from bs4 import BeautifulSoup
+import requests, feedparser, os, time
+
+#list of models, subject to your preference
+models_name = ['Park Jung Yoon',
+			   'An Seo Rin', 
+               ]
+
+success = False
+
+#initiate feedparser on rss
+cqrss = feedparser.parse('https://www.reddit.com/r/cutekorean/new.rss')
+
+while not success:
+    try:
+        for submission in cqrss.entries:
+            for model in models_name:
+                if model.lower() in submission.title.lower():
+                    folder_name = submission.title #use for create folder
+                    reddit_url = submission.link
+                    source = requests.get(reddit_url)
+                    plain_text = source.content
+                    soup = BeautifulSoup(plain_text, 'lxml')
+                    title = soup.find('a', 'title may-blank outbound', href=True)
+                    if title and 'imgur.com' in title['href']:
+                        imgur_link = title['href'] #album download link
+                        dl_command = "python imgurdownloader.py " + imgur_link
+                        print("Start downloading " + folder_name + ".")
+                        os.system(dl_command)
+                        print(folder_name + " download finishied.")
+        success = True
+    except TypeError:
+        pass
+        print("An error has appeared. Let's try again in 3, 2, 1~")
+        time.sleep(3)
+                
+print("Mission Complete!")
